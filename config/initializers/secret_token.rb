9c00f3b93582e4d319708bbb1bc8bdf6e130e83b
blablo <blablo@gmail.com>
2012-04-20 17:36:52 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PalabraFiel::Application.config.secret_token = '27f968e888a7a23f64f9de291746d416edb9910421c1c17c6a668e4b0a3184bc6730aad0aea3cecd6afeea3163eca53c59d66399a6fe17ea6cc75829124750b5'
