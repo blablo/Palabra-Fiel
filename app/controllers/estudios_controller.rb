5fd66d937b249de78a41dd9645381b55e96e7a14
blablo <blablo@gmail.com>
2012-05-11 18:45:47 -0500

    edit

--- app/controllers/estudios_controller.rb
+++ app/controllers/estudios_controller.rb
@@ -10,13 +10,29 @@ class EstudiosController < ApplicationController
   def new
     @estudio = Estudio.new
   end
-  
+
+  def edit
+    @estudio = Estudio.find(params[:id])
+  end
 
   def create
     @estudio = Estudio.new(params[:estudio])
 
     respond_to do |format|
       if @estudio.save
+        format.html { redirect_to(@estudio, :notice => 'Estudio was successfully created.') }
+        format.xml  { render :xml => @estudio, :status => :created, :location => @estudio }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @estudio.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  def update
+    @estudio = Estudio.find(params[:id])
+
+    respond_to do |format|
+      if @estudio.update_attributes(params[:estudio])
         format.html { redirect_to(@estudio, :notice => 'Estudio was successfully created.') }
         format.xml  { render :xml => @estudio, :status => :created, :location => @estudio }
       else
